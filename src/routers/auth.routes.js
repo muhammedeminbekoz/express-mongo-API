4db90a64d3ad8d179fa93a3ca1dd0eb5ec1050d8
muhammedeminbekoz <m.eminbekoz19@gmail.com>
2024-05-17 23:50:19 +0300

    feat: Add authValidation middleware to register route

--- src/routers/auth.routes.js
+++ src/routers/auth.routes.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const { login, register } = require('../controllers/auth.controller');
-
+const authValidation = require('../middlewares/validation/auth.validation')
 
 router.post('/login', login);
-router.post('/register', register);
+router.post('/register', authValidation.register, register);
 
 
 module.exports = router;
