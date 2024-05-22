f32b44f4e0466dd8e910b7224ea1f050a4784d78
muhammedeminbekoz <m.eminbekoz19@gmail.com>
2024-05-22 23:38:38 +0300

    feat: Add login validation middleware

--- src/middlewares/validation/auth.validation.js
+++ src/middlewares/validation/auth.validation.js
@@ -45,7 +45,32 @@ class authValidation {
         }
 
     }
-
+    static login = async (req, res, next) => {
+        try {
+            await joi.object({
+                email: joi.string().email().trim().min(3).max(50).required().messages({
+                    "string.base": "email alanı metin olmalıdır",
+                    "string.empty": "email alanı boş olamaz",
+                    "string.min": "email alanı en az 3 karakterden oluşmalıdır",
+                    "string.email": "lütfen geçerli bir email giriniz",
+                    "string.max": "email alanı en fazla 50 karakterden oluşmalıdır",
+                    "string.required": "email alanı zorunludur"
+                }),
+                password: joi.string().trim().min(6).max(50).required().messages({
+                    "string.base": "parola alanı metin olmalıdır",
+                    "string.empty": "parola alanı boş olamaz",
+                    "string.min": "parola alanı en az 6 karakterden oluşmalıdır",
+                    "string.max": "parola alanı en fazla 50 karakterden oluşmalıdır",
+                    "string.required": "parola alanı zorunludur"
+                })
+            }).validateAsync(req.body);
+        }
+        catch (error) {
+            if (error && error?.details[0].message) throw new APIError(error?.details[0].message, 400)
+            else throw new APIError("lütfen validasyon kurallarına uyunuz", 400)
+        }
+        next();
+    }
 }
 
 module.exports = authValidation;
