57cd1bacafc8ffb17ac589efeeb27b440c3232e0
muhammedeminbekoz <m.eminbekoz19@gmail.com>
2024-05-29 08:59:51 +0300

    feat: Add tokenCheck middleware for JWT token verification

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,4 +1,6 @@
 const jwt = require('jsonwebtoken');
+const APIError = require('../utils/errors');
+const User = require('../models/user.model');
 
 const createToken = async (user, res) => {
     const payload = {
@@ -12,7 +14,29 @@ const createToken = async (user, res) => {
     res.status(201).json({ success: true, token, message: "Başarılı" });
 }
 
+const tokenCheck = async (req, res, next) => {
+    const headerToken = req?.headers?.authorization?.startsWith("Bearer")
+    if (!headerToken)
+        throw new APIError("Geçersiz oturum litfen Oturum açın", 401)
+
+    const token = req.headers.authorization.split(" ")[1]
+
+    await jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, decoded) => {
+        if (err) throw new APIError("Geçersiz Token", 401)
+
+        const userInfo = await User.findById(decoded.sub).select("_id firstname lastname email")
+
+        if (!userInfo) throw new APIError("Geçersiz token", 401)
+
+        req.user = userInfo;
+
+    })
+    next();
+
+
+}
 
 module.exports = {
-    createToken
+    createToken,
+    tokenCheck
 }
