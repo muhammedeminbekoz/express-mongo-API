d6976fd480344c9e02009fe6d4c8ad35778b347e
muhammedeminbekoz <m.eminbekoz19@gmail.com>
2024-05-29 09:00:07 +0300

    feat: Add "me" endpoint and tokenCheck middleware for user authentication

--- src/routers/auth.routes.js
+++ src/routers/auth.routes.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
-const { login, register } = require('../controllers/auth.controller');
-const authValidation = require('../middlewares/validation/auth.validation')
+const { login, register, me } = require('../controllers/auth.controller');
+const authValidation = require('../middlewares/validation/auth.validation');
+const { tokenCheck } = require('../middlewares/auth.js');
 
 router.post('/login', authValidation.login, login);
 router.post('/register', authValidation.register, register);
-
+router.get('/me', tokenCheck, me)
 
 module.exports = router;
