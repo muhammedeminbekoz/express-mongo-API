8ba14376d7d8d1258cecd4f9b6b3fce9a74e76c2
muhammedeminbekoz <m.eminbekoz19@gmail.com>
2024-05-30 23:53:39 +0300

    feat: Added CORS middleware to handle cross-origin requests

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 require('./src/db/dbConnection');
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 const router = require('./src/routers/index');
 const errorHandlerMiddleware = require('./src/middlewares/errorHandler')
+const cors = require('cors');
+const corsOptions = require('./src/helpers/corsOptions');
 
 //middlewares
+app.use(cors(corsOptions))
 app.use(express.json())
 
 app.get('/', (req, res) => {
