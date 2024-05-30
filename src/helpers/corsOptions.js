d4d8f16370dd46b1fe90d5c394606a13e83fd369
muhammedeminbekoz <m.eminbekoz19@gmail.com>
2024-05-30 23:51:58 +0300

    feat: Add corsOptions helper function for CORS configuration

---
+++ src/helpers/corsOptions.js
@@ -0,0 +1,22 @@
+const whiteList = ["http://localhost:1903", "http://localhost:3000"]
+
+const corsOptions = (req, callback) => {
+    let corsOptions;
+    console.log('All headers:', req.headers);  // Log all headers
+    console.log(req.headers.host)
+    const origin = req.header("Origin");
+    console.log('Origin:', origin);  // Log the Origin header
+
+    if (whiteList.indexOf(origin) !== -1) {
+        console.log("Origin allowed");
+        corsOptions = { origin: true };
+    } else {
+        console.log("Origin not allowed");
+        corsOptions = { origin: false };
+    }
+
+    callback(null, corsOptions);
+};
+
+
+module.exports = corsOptions
