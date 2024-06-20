61c186d42c7c24398b37e96b4d477d0fefa2c0f0
muhammedeminbekoz <m.eminbekoz19@gmail.com>
2024-06-20 15:46:34 +0300

    feat: Add file upload endpoint using multer middleware

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,7 +1,30 @@
 const router = require('express').Router();
 const auth = require('./auth.routes');
+const upload = require('../middlewares/lib/upload');
+const multer = require('multer');
+const APIError = require('../utils/errors');
+
 
 router.use(auth);
+
+
+
+router.post('/upload', (req, res) => {
+    upload(req, res, function (err) {
+        if (err instanceof multer.MulterError) {
+            console.log("hata: ", err)
+            throw new APIError("Resim yüklenirken multer kaynaklı bir hata meydana geldi", err)
+        }
+        else if (err) {
+            throw new APIError("Resim yüklenirken bir hata oluştu", err)
+        }
+        else {
+            return res.status(200).json({ message: "başarılı" })
+        }
+    })
+
+})
+
 
 
 
