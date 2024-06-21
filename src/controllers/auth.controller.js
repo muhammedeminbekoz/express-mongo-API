d20b536a1f43bb9d96573bb6303d01394db96e17
muhammedeminbekoz <m.eminbekoz19@gmail.com>
2024-06-21 10:48:02 +0300

    feat: Add forget password functionality with email notification

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,6 +3,9 @@ const bcrypt = require('bcrypt');
 const APIError = require('../utils/errors');
 const Response = require('../utils/response');
 const { createToken } = require('../middlewares/auth');
+const crypto = require('crypto')
+const sendEmail = require('../utils/sendMail');
+const moment = require('moment');
 
 const login = async (req, res) => {
     const { email, password } = req.body;
@@ -46,8 +49,41 @@ const me = async (req, res) => {
     return new Response(req.user).success(res)
 }
 
+
+const forgetPassword = async (req, res) => {
+    const { email } = req.body
+
+    const userInfo = await User.findOne({ email }).select(" name lastname email ")
+
+    if (!userInfo) return new APIError("Geçersiz Kullanıcı", 400)
+
+    console.log("userInfo : ", userInfo);
+
+    const resetCode = crypto.randomBytes(3).toString("hex")
+
+    await sendEmail({
+        from: process.env.EMAIL_AUTH_USER,
+        to: userInfo.email,
+        subject: "Şifre Sıfırlama",
+        text: `Şifre Sıfırlama Kodunuz ${resetCode}`
+    })
+
+    await User.updateOne(
+        { email },
+        {
+            reset: {
+                code: resetCode,
+                time: moment(new Date()).add(15, "minute").format("YYYY-MM-DD HH:mm:ss")
+            }
+        }
+    )
+
+    return new Response(true, "Lütfen Mail Kutunuzu Kontrol Ediniz").success(res)
+}
+
 module.exports = {
     login,
     register,
-    me
+    me,
+    forgetPassword
 }
