244de821d85f83c9803b2bfe618c44ae52338eca
muhammedeminbekoz <m.eminbekoz19@gmail.com>
2024-06-21 10:52:18 +0300

    feat: Add forget password endpoint with email notification

--- src/routers/auth.routes.js
+++ src/routers/auth.routes.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
-const { login, register, me } = require('../controllers/auth.controller');
+const { login, register, me, forgetPassword } = require('../controllers/auth.controller');
 const authValidation = require('../middlewares/validation/auth.validation');
 const { tokenCheck } = require('../middlewares/auth.js');
 
 router.post('/login', authValidation.login, login);
 router.post('/register', authValidation.register, register);
 router.get('/me', tokenCheck, me)
+router.post('/forgetpassword', forgetPassword)
 
 module.exports = router;
