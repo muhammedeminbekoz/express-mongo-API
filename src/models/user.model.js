8431e914c9ccf2c5c63205e2e1f2f5a4171cf436
muhammedeminbekoz <m.eminbekoz19@gmail.com>
2024-06-21 10:52:08 +0300

    refactor: Add reset code and time fields to user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -21,6 +21,16 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         trim: true
+    },
+    reset: {
+        code: {
+            type: String,
+            default: null,
+        },
+        time: {
+            type: Date,
+            default: null
+        }
     }
 
 
